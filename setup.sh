db881ae8d99eb726899ce27f59b19f347c8316e5
powwow787 <t15789647@gmail.com>
2026-02-15 23:25:49 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 #ARCH + WAYBAR + WBG + FCITX5 + HYPRLAND + PIPEWIRE + PYWAL
 
 #MAKE SURE YOU ARE CONNECTED WITH WIFI and BE SURE TO BE SET SUDO WELL.
 set -e
 
+HYPR_DIR=/home/powwow/.config/hypr
+DOTFILE=https://github.com/elifouts/Dotfiles
+DOTFILE_DIR=~/.config/
+YAY_DIR=~/yay
+WP_SOURCE=~/arch-setup/wp/*
+
 echo "Starting to install things"
-sudo pacman -S  --needed ttf-jetbrains-mono-nerd waybar fcitx5 fcitx5-hangul fcitx5-configtool wayland hyprland pipewire wireplumber pywal alsa-utils noto-fonts-cjk noto-fonts git python-pywal ly network-manager-applet base-devel alacritty dolphin wofi pipewire-alsa pipewire-pulse
+sudo pacman -S  --needed ttf-jetbrains-mono-nerd waybar fcitx5 fcitx5-hangul fcitx5-configtool wayland hyprland pipewire wireplumber python-pywal noto-fonts-cjk noto-fonts git python-pywal network-manager-applet base-devel alacritty nnn wofi pipewire-alsa pipewire-pulse
 
 echo "hyprland setting..."
-mkdir -pv ~/.config/hypr
+mkdir -pv $HYPR_DIR
 
-cd ~/.config/hypr
+cd $HYPR_DIR
 
-git clone https://github.com/powwow787/dotfiles
+git clone https://github.com/powwow787/dotfiles | true #since i created a file that makes error, but i dont wanna change it 'cause i dont wanna it.
 
 rm -rf *.nix
 
 echo "hyprland set."
 
 echo "waybar setting..."
-git clone https://github.com/elifouts/Dotfiles ~/.config/waybar
-cd ~/.config/waybar
-rm -rf .bashrc LICENSE readme.md wallpapers images InstallScripts
-cd .config
-mv waybar ..
-cd ..
-rm -rf .config
+git clone $DOTFILE $DOTFILE_DIR
+cd $DOTFILE_DIR
+mv .config/waybar ~/.config/waybar
 
 echo "waybar set."
 
@@ -41,10 +43,11 @@ echo "i18n set right."
 
 echo "First, we install wbg, we have to install yay for first."
 
-git clone https://aur.archlinux.org/yay-bin
-cd yay-bin
+git clone https://aur.archlinux.org/yay-bin $YAY_DIR
+cd $YAY_DIR
 makepkg -si
-
+cd ~/
+rm -rf $YAY_DIR
 echo "yay installed."
 
 echo "installing wbg"
@@ -61,15 +64,15 @@ echo "pipewire set"
 
 echo "setting another things"
 
-sudo systemctl enable ly
+#sudo systemctl enable ly
 cd ~/
 
 mkdir .wallpaper
-cd .wallpaper
+mv $WP_SOURCE ~/.wallpaper
 #curl -O https://url.kr/pxn2xc 
 #mv *.png wallpaper.png
 
-wal -i ./*.png
+wal -i ~/.wallpaper/*.png
 
 clear
 
